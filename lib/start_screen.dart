fa1b22ade365b0157f7bd69b25bf48a9ccd6329e
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-06-20 08:47:48 +0900

    スタート画面のレイアウトを作成

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -6,8 +6,25 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Text('Start Screen'),
-    ));
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Edit Snap'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'こんにちは！\n今日は${DateTime.now()}です。',
+              textAlign: TextAlign.center,
+            ),
+            ElevatedButton(
+              child: const Text('開始する'),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
