1b4ab42b44554c2b503b76e144da7cc9921d346f
hide0621 <fujihara_hideyuki@retail-ai.jp>
2024-06-26 15:52:11 +0900

    スマホの画像ライブラリから画像を取得する処理を実装

--- lib/image_select_screen.dart
+++ lib/image_select_screen.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:image/image.dart' as image_lib;
+import 'package:image_picker/image_picker.dart';
 
 class ImageSelectScreen extends StatefulWidget {
   const ImageSelectScreen({super.key});
@@ -8,6 +11,50 @@ class ImageSelectScreen extends StatefulWidget {
 }
 
 class _ImageSelectScreenState extends State<ImageSelectScreen> {
+  /// ImagePicker
+  /// image_pickerパッケージが提供するクラス 画像ライブラリやカメラにアクセスする機能を持つ
+  final ImagePicker _picker = ImagePicker();
+
+  /// Uint8List
+  /// 8bit 符号なし整数のリスト
+  Uint8List? _imageBitmap;
+
+  Future<void> _selectImage() async {
+    /// XFile
+    /// ファイルの抽象化クラス
+
+    /// 画像を選択する
+    final XFile? imageFile =
+        await _picker.pickImage(source: ImageSource.gallery);
+
+    /// ファイルオブジェクトから画像データを取得する
+    final imageBitmap = await imageFile?.readAsBytes();
+    assert(imageBitmap != null);
+    if (imageBitmap == null) return;
+
+    /// 画像データをデコードする
+    final image = image_lib.decodeImage(imageBitmap);
+    assert(image != null);
+    if (image == null) return;
+
+    /// Image
+    /// 画像データとメタデータを内包したクラス
+    /// 下記はimageパッケージのImageクラスを参照している
+    final image_lib.Image resizedImage;
+    if (image.width > image.height) {
+      /// 横長の画像なら横幅を500pxにリサイズする
+      resizedImage = image_lib.copyResize(image, width: 500);
+    } else {
+      /// 縦長の画像なら縦幅を500pxにリサイズする
+      resizedImage = image_lib.copyResize(image, height: 500);
+    }
+
+    /// 画像をエンコードして状態を更新する
+    setState(() {
+      _imageBitmap = image_lib.encodeBmp(resizedImage);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final l10n = L10n.of(context);
